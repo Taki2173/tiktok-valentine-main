f08ec484c1dd7cbf6ed524fe87ae9f3dbdc02edd
Taki2173 <invitaraamigos@gmail.com>
2025-02-02 12:14:05 -0500

    Actualización de vite.config.js con configuraciones extendidas

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // Asegura que las rutas sean relativas en producción
+  base: './', // Define rutas relativas en producción
+  build: {
+    outDir: 'dist', // Carpeta donde se generarán los archivos de producción
+    assetsDir: 'assets', // Carpeta donde se guardarán los assets como imágenes, CSS, JS
+    sourcemap: true, // Activa el sourcemap para depurar errores en producción
+  },
+  server: {
+    open: true, // Abre el navegador automáticamente al ejecutar el servidor de desarrollo
+  },
 });
