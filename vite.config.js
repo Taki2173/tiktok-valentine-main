a04a32df88a0bd1b746f63b3ee63750adfe8ee25
Taki2173 <invitaraamigos@gmail.com>
2025-02-02 11:14:50 -0500

    Ajustando rutas relativas y configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // Configuración para rutas relativas
+  base: './', // Rutas relativas para los archivos estáticos
 });
